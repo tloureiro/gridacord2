2e10333cd84d5580353194d238104b77114aebe3
gorgotron <thiago@artscience.ca>
2017-01-20 01:34:12 -0500

    lint errors gone

--- gridacord.js
+++ gridacord.js
@@ -1,7 +1,5 @@
 /* global $ */
 
-//TODO fix lint errors
-
 $(function () {
   var target = $('#gridacord-container');
   var newExpandedWidthPct;
@@ -14,7 +12,8 @@ $(function () {
   var itemsQtd = $('.item', target).length;
   var itemsQtdSquareRoot = Math.sqrt(itemsQtd);
   var itemsQtdSquareRootMinusOne = Math.sqrt(itemsQtd) - 1;
-  var itemSize;
+  var itemSizePct;
+  var itemSizePixels;
 
   /**
    * TODO: implement this function
@@ -28,12 +27,11 @@ $(function () {
     $('.item', target).each(function (ignore, item) {
       var focusX = $(item).attr('data-focus-x') ? $(item).attr('data-focus-x') : 50;
       var focusY = $(item).attr('data-focus-y') ? $(item).attr('data-focus-y') : 50;
+      var newImageFocusX;
+      var newImageFocusY;
 
-      var itemWidth = $(item).width();
-      var itemHeight = $(item).height();
-
-      var newImageFocusX = ($('img', item).get(0).naturalWidth * (focusX / 100)) - (itemWidth / 2);
-      var newImageFocusY = ($('img', item).get(0).naturalHeight * (focusY / 100)) - (itemHeight / 2);
+      newImageFocusX = ($('img', item).get(0).naturalWidth * (focusX / 100)) - (itemSizePixels / 2);
+      newImageFocusY = ($('img', item).get(0).naturalHeight * (focusY / 100)) - (itemSizePixels / 2);
 
       $('img', item).animate({ top: '-' + newImageFocusY + 'px',
         left: '-' + newImageFocusX + 'px' }, { duration: transition ? transitionTime : 0, queue: false });
@@ -46,11 +44,12 @@ $(function () {
     });
   }
 
-  function initGrid(itemsQtdSquareRoot) {
-    itemSize = (100 / itemsQtdSquareRoot) + '%';
+  function initGrid() {
+    itemSizePct = (100 / itemsQtdSquareRoot) + '%';
+    itemSizePixels = $('.item', target).eq(0).width();
 
-    $('.line', target).css('height', itemSize);
-    $('.item', target).css('width', itemSize);
+    $('.line', target).css('height', itemSizePct);
+    $('.item', target).css('width', itemSizePct);
     $('.item img', target).each(function (ignore, value) {
       $(value).css('width', $(this).get(0).naturalWidth).css('height', $(this).get(0).naturalHeight);
     });
@@ -58,15 +57,13 @@ $(function () {
     focusImages(false);
   }
 
-  function resizeItems(selectedItemX, selectedItemY) {
-
-
+  function resizeItemsFocus(selectedItemX, selectedItemY) {
     var imgWidth = $('.item img', $('.line', target).eq(selectedItemY)).eq(selectedItemX).width();
     var imgHeight = $('.item img', $('.line', target).eq(selectedItemY)).eq(selectedItemX).height();
-    var x, y;
+    var x;
+    var y;
 
     if (imgWidth > imgHeight) {
-
       newExpandedWidthPct = maxRatio;
       newRemainingWidthPct = (100 - maxRatio) / itemsQtdSquareRootMinusOne;
 
@@ -74,9 +71,8 @@ $(function () {
       newRemainingHeightPct = (100 - newExpandedHeightPct) / itemsQtdSquareRootMinusOne;
 
       $('.item', $('.line', target).eq(selectedItemY)).eq(selectedItemX).animate(
-        {'width': '100%', 'height': '100%'}, {'duration': transitionTime, 'queue': false}
+        { width: '100%', height: '100%' }, { duration: transitionTime, queue: false }
       );
-
     } else {
       newExpandedHeightPct = maxRatio;
 
@@ -85,90 +81,69 @@ $(function () {
       newRemainingWidthPct = (100 - newExpandedWidthPct) / itemsQtdSquareRootMinusOne;
 
       $('.item', $('.line', target).eq(selectedItemY)).eq(selectedItemX).animate(
-        {'height': '100%', 'width': '100%'}, {'duration': transitionTime, 'queue': false}
+        { height: '100%', width: '100%' }, { duration: transitionTime, queue: false }
       );
     }
 
     focusImages(true);
 
-    for (y = 0; y < itemsQtdSquareRoot; y+=1) {
-
-      for (x = 0; x < itemsQtdSquareRoot; x+=1) {
-
-
+    for (y = 0; y < itemsQtdSquareRoot; y += 1) {
+      for (x = 0; x < itemsQtdSquareRoot; x += 1) {
         if (y === selectedItemY && x === selectedItemX) {
-
-          $('.item', $('.line', target).eq(y)).eq(x).animate({'width': newExpandedWidthPct + '%'}, {
-            'duration': transitionTime,
-            'queue': false
+          $('.item', $('.line', target).eq(y)).eq(x).animate({ width: newExpandedWidthPct + '%' }, {
+            duration: transitionTime,
+            queue: false
           });
-
         } else if (y === selectedItemY && x !== selectedItemX) {
-
-          $('.item', $('.line', target).eq(y)).eq(x).animate({'width': newRemainingWidthPct + '%'}, {
-            'duration': transitionTime,
-            'queue': false
+          $('.item', $('.line', target).eq(y)).eq(x).animate({ width: newRemainingWidthPct + '%' }, {
+            duration: transitionTime,
+            queue: false
           });
-
         } else if (y !== selectedItemY && x === selectedItemX) {
-
-          $('.item', $('.line', target).eq(y)).eq(x).animate({'width': newExpandedWidthPct + '%'}, {
-            'duration': transitionTime,
-            'queue': false
+          $('.item', $('.line', target).eq(y)).eq(x).animate({ width: newExpandedWidthPct + '%' }, {
+            duration: transitionTime,
+            queue: false
           });
-
         } else {
-
-          $('.item', $('.line', target).eq(y)).eq(x).animate({'width': newRemainingWidthPct + '%'}, {
-            'duration': transitionTime,
-            'queue': false
+          $('.item', $('.line', target).eq(y)).eq(x).animate({ width: newRemainingWidthPct + '%' }, {
+            duration: transitionTime,
+            queue: false
           });
         }
-
       }
 
       if (y === selectedItemY) {
-        $('.line', target).eq(y).animate({'height': newExpandedHeightPct + '%'}, {
-          'duration': transitionTime,
-          'queue': false
+        $('.line', target).eq(y).animate({ height: newExpandedHeightPct + '%' }, {
+          duration: transitionTime,
+          queue: false
         });
       } else {
-
-        $('.line', target).eq(y).animate({'height': newRemainingHeightPct + '%'}, {
-          'duration': transitionTime,
-          'queue': false
+        $('.line', target).eq(y).animate({ height: newRemainingHeightPct + '%' }, {
+          duration: transitionTime,
+          queue: false
         });
       }
     }
   }
 
-  if ( isGridValid() ) {
+  if (isGridValid()) {
     initGrid(itemsQtdSquareRoot);
-
   }
 
-
   $('.item', target).hover(function () {
-
     var index = $('.item', target).index(this);
 
-    var y = Math.floor(index / itemsQtdSquareRoot);
-    var x = Math.floor(index % itemsQtdSquareRoot);
+    var y = parseInt(index / itemsQtdSquareRoot, 10);
+    var x = parseInt(index % itemsQtdSquareRoot, 10);
 
-    resizeItems(x, y);
+    resizeItemsFocus(x, y);
     unfocusImages();
-
   }, function () {
     focusImages(true);
 
-    // $('img',this).css('max-width', 'none').css('max-height', 'none');
     //TODO move this to a function
-    $('img', this).animate({
-      'width': $('img', this).get(0).naturalWidth,
-      'height': $('img', this).get(0).naturalHeight
-    }, {'duration': transitionTime, 'queue': false});
-    $('.item', target).animate({'width': itemSize}, {'duration': transitionTime, 'queue': false});
-    $('.line', target).animate({'height': itemSize}, {'duration': transitionTime, 'queue': false});
+    $('.item', target).animate({ width: itemSizePct }, { duration: transitionTime, queue: false });
+    $('.line', target).animate({ height: itemSizePct }, { duration: transitionTime, queue: false });
   });
 
 });
